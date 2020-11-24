f5e41c5b671e100c956b5ed45ef0c64a5f5b9d10
Ohta Akira <waterlow3442@gmail.com>
2020-11-25 01:12:21 +0900

    色をつけてわかりやすく

--- src/column.jsx
+++ src/column.jsx
@@ -9,10 +9,11 @@ const Container = styled.div`
   border-radius: 2px;
 `;
 const Title = styled.h3`
-  padding: 8px
+  padding: 8px;
 `
 const TaskList = styled.div`
-  padding: 8px
+  padding: 8px;
+  background-color: ${props => (props.isDraggingOver ? 'skyblue' : 'white')};
 `
 
 export default class Column extends React.Component {
@@ -21,10 +22,11 @@ export default class Column extends React.Component {
       <Container>
         <Title>{this.props.column.title}</Title>
         <Droppable droppableId={this.props.column.id}>
-          {(provided) => (
+          {(provided, snapshot) => (
             <TaskList
               ref={provided.innerRef}
               {...provided.droppableProps}
+              isDraggingOver={snapshot.isDraggingOver}
             >
               {this.props.tasks.map((t, i) => <Task key={t.id} task={t} index={i} />)}
               {provided.placeholder}
